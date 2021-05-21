80e64921a8352eefd212b0ca3d406e7a87bfb56e
Thomas Li <77999105+ThomasDLi@users.noreply.github.com>
2021-05-21 12:13:29 -0700

    Add files via upload

---
+++ impastasus.py
@@ -0,0 +1,41 @@
+from PIL import Image, ImageEnhance
+
+user_account_name = "Thomas.Li26"
+
+def main():
+    mode = input("Specify image editing mode. Type DEEPFRY, STRETCH, or INVERT: ")
+    if mode == "DEEPFRY":
+        DEEPFRY()
+    if mode == "STRETCH":
+        STRETCH()
+    if mode == "INVERT":
+        INVERT()
+
+def DEEPFRY():
+    img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
+    im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
+    enhancer = ImageEnhance.Contrast(im)
+    factor = float(input("Specify deepfry amount (0-100): "))
+    im_output = enhancer.enhance(factor)
+    im_output.save('more-contrast-image.png')
+    im_output.show()
+
+def STRETCH():
+    img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
+    im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
+    factor = int(input("Specify width: "))
+    factor2 = int(input("Specify height: "))
+    im_output = im.resize((factor,factor2))
+    im_output.save('more-contrast-image.png')
+    im_output.show()
+
+def INVERT():
+    img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
+    im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
+    enhancer = ImageEnhance.Contrast(im)
+    im_output = enhancer.enhance(-1)
+    im_output.save('more-contrast-image.png')
+    im_output.show()
+
+if __name__ == "__main__":
+    main()
