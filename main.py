41058351996cfe8d79701d6972cf62bf7b8cab58
Thomas Li <77999105+ThomasDLi@users.noreply.github.com>
2021-05-21 12:37:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from PIL import Image, ImageEnhance
 user_account_name = "Thomas.Li26"
 
 def main():
-    mode = input("Specify image editing mode. Type DEEPFRY, STRETCH, or INVERT: ")
+    mode = input("Specify image editing mode. Type DEEPFRY, STRETCH, BRIGHTNESS, SHARPEN, or INVERT: ")
     if mode == "DEEPFRY":
         DEEPFRY()
     if mode == "STRETCH":
         STRETCH()
     if mode == "INVERT":
         INVERT()
+    if mode == "BRIGHTNESS":
+        BRIGHTNESS()
+    if mode == "SHARPEN":
+        SHARPEN()
 
 def DEEPFRY():
     img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
@@ -34,6 +38,24 @@ def INVERT():
     im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
     enhancer = ImageEnhance.Contrast(im)
     im_output = enhancer.enhance(-1)
+    im_output.save('more-contrast-image.png')
+    im_output.show()
+
+def BRIGHTNESS():
+    img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
+    im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
+    enhancer = ImageEnhance.Brightness(im)
+    factor = float(input("Specify brightness amount: "))
+    im_output = enhancer.enhance(factor)
+    im_output.save('more-contrast-image.png')
+    im_output.show()
+
+def SHARPEN():
+    img = input("Insert the name of an image found in the Downloads folder (for example: Image.png): ")
+    im = Image.open(r"C:\Users\{}\Downloads\{}".format(user_account_name, img))
+    enhancer = ImageEnhance.Sharpness(im)
+    factor = float(input("Specify sharpening amount: "))
+    im_output = enhancer.enhance(factor)
     im_output.save('more-contrast-image.png')
     im_output.show()
 
